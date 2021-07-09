57e29c84f4f04731918bc68886abdedd2468b78d
sleepingsamurai <anubhav.singh.met19@itbhu.ac.in>
2021-07-09 21:38:09 +0530

    done for now

--- api/serializers.py
+++ api/serializers.py
@@ -8,7 +8,7 @@ from rest_framework.authtoken.views import Token
 class UserSerializer(serializers.ModelSerializer) :
     class Meta:
         model = User
-        fields = ['username','email', 'password']
+        fields = ['id','username','email', 'password']
 
         extra_kwargs = {'password':{
             'write_only': True,
